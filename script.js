0c282bf5d41479ea37efbde518142775ba118092
heena <parm0058@algonquinlive.com>
2020-01-09 19:47:02 -0500

    add script

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let name = 'Hello my name is heena parmar'
-console.log(name)
+let name = 'heena parmar'
+console.log(`hello my name is ${name}`)
 const number = 8
+console.log(`this is printing number ${number}`)
 let multiply = number * 2
 multiply = multiply * 5
 multiply = multiply / number
 multiply = multiply - 7
-console.log(multiply)
+console.log(`After processing output ${multiply}`)
