668b6c9dcf925b7f3edef1d577ba46714c2df7d1
heena <parm0058@algonquinlive.com>
2020-01-09 19:04:41 -0500

    add html

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = 'Hello my name is heena parmar'
+console.log(name)
+const number = 5
+let multiply = number * 2
+multiply = multiply * 5
+multiply = multiply / number
+multiply = multiply - 7
+console.log(multiply)
